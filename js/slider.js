efe05cdad916b0d62c6a8239ad242b0e72d9036a
Moha <104149341+Mohajiro@users.noreply.github.com>
2022-05-28 15:03:43 +0200

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".swiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+      },
+  });
